d732986e15b7060022f74bcf1aec6aedd2149059
Ido Shemesh <ido.shemesh001@gmail.com>
2019-06-24 18:36:07 -0700

    add Sentry browser JS

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const Sentry = require('@sentry/browser');
 
-function sum(a, b) {
+
+
+/*function sum(a, b) {
     return a + b;
+}*/
+
+function testCaptureException(callback){
+    Sentry.init({
+        dsn: 'https://825d3877df6c46138114d45066925eb5@sentry.io/1489337',
+        beforeSend(event, hint) {
+            callback(event);
+            //return event;
+          }
+    });
+
+    Sentry.captureException(new Error("new error"));
 }
 
 
-module.exports = sum;
+module.exports = {
+    //sum: sum,
+    testCaptureException: testCaptureException
+}
--- trial.test.js
+++ trial.test.js
@@ -1,5 +1,17 @@
-const sum = require('./index');
+const entryPoint = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+/*test('adds 1 + 2 to equal 3', () => {
+  expect(entryPoint.sum(1, 2)).toBe(3);
+});
+*/
+
+
+test('Test Sentry.captureException', done => {
+  function callback(event) {
+    
+    expect(event.exception.values[0].value).toBe('new error');
+    done();
+  }
+
+  entryPoint.testCaptureException(callback);
 });
